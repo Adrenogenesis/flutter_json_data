a4949f7c201f0f69e29837f0de09564a2747f567
protonwebmaster <exonet3i@gmail.com>
2021-03-26 11:42:59 +0100

    dbupdate commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,24 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> createAlbum(String title) async {
-  final response = await http.post(
-    Uri.https('https://my-json-server.typicode.com/protonwebmaster/flutter_json_data', 'albums'),
+Future<Album> fetchAlbum() async {
+  final response =
+  await http.get(Uri.https('jsonplaceholder.typicode.com', 'albums/1'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+Future<Album> updateAlbum(String title) async {
+  final http.Response response = await http.put(
+    Uri.https('jsonplaceholder.typicode.com', 'albums/1'),
     headers: <String, String>{
       'Content-Type': 'application/json; charset=UTF-8',
     },
@@ -15,10 +30,14 @@ Future<Album> createAlbum(String title) async {
     }),
   );
 
-  if (response.statusCode == 201) {
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
     return Album.fromJson(jsonDecode(response.body));
   } else {
-    throw Exception('Failed to create album.');
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to update album.');
   }
 }
 
@@ -54,44 +73,51 @@ class _MyAppState extends State<MyApp> {
   Future<Album> _futureAlbum;
 
   @override
+  void initState() {
+    super.initState();
+    _futureAlbum = fetchAlbum();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Create Data Example',
+      title: 'Update Data Example',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Create Data Example'),
+          title: Text('Update Data Example'),
         ),
         body: Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.all(8.0),
-          child: (_futureAlbum == null)
-              ? Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: _controller,
-                decoration: InputDecoration(hintText: 'Enter Title'),
-              ),
-              ElevatedButton(
-                child: Text('Create Data'),
-                onPressed: () {
-                  setState(() {
-                    _futureAlbum = createAlbum(_controller.text);
-                  });
-                },
-              ),
-            ],
-          )
-              : FutureBuilder<Album>(
+          child: FutureBuilder<Album>(
             future: _futureAlbum,
             builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Text(snapshot.data.title);
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(snapshot.data.title),
+                      TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(hintText: 'Enter Title'),
+                      ),
+                      ElevatedButton(
+                        child: Text('Update Data'),
+                        onPressed: () {
+                          setState(() {
+                            _futureAlbum = updateAlbum(_controller.text);
+                          });
+                        },
+                      ),
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
+                }
               }
 
               return CircularProgressIndicator();
